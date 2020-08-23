743fe826150925ca50179eeda45357325ad2012c
Jared DL <davisleyton@outlook.com>
2020-08-23 17:19:46 -0400

    Añadi todos los libros que leí, osea puse la data con la que trabajaré

--- diseño.php
+++ diseño.php
@@ -8,9 +8,6 @@
         <p> A continuación se desarrollará un pequeño sitio web con todos los libros que <br> quiera leer en un futuro, además de los libros que ya leí. </p>
         
         <?php
-        include_once('clases.php');
-        include_once('funciones.php');
-        include_once('variables.php');
         include_once('programa.php');
         ?>
 
--- variables.php
+++ variables.php
@@ -1,4 +1,41 @@
 <?php
 include_once('clases.php');
 $books[0]=new Book("El mundo de sofía" , "Jostein Gaarder" , "Español" , true , "Ya había intentado leer este libro antes, pero no había llamado la atención. Finalmene por curiosidad decidí leer este libro" , "Creo que nunca antes había sentido como un libro es capaz de enseñarte tanto") ;
- 
+
+$books[1]=new Book("Aprender a vivir" , "Luc Ferry" , "Español" , true , "No sabía que esperar, solo me dio curiosidad porque mi hermano me lo recomendo" , "Este fue el libro que me inserntivo a leer mas porque no creía que un libro fuera capaz de enseñarme tanto") ;
+
+$books[2]=new Book("Ética para amador" , "Fernando Savater" , "Español" , true , "Investigando en internet halle este libro entre otros, al ir a buscar libros que comprar con mi listita este fue el único que encontré" , "Fernando Savater enseña a su hijo como ser ético, lo que mas me sorprendió fue el hecho de que incluye a Dios para ser bueno, algo que en su momento me fue muy extraño.") ;
+
+$books[3]=new Book("Los 10 mandamientos y su vegencia en el siglo XX" , "Fernando Savater" , "Español" , true , "Este lo compre junto con Ética para amador, porque no encontré mas libros" , "Este fue el que realmente puso en cuerda floja mi creencia en Dios debido a la forma en como analiza cada uno de los diez mandamientos") ;
+
+$books[4]=new Book("Política para Amador" , "Fernando Savater" , "Español" , true , "Tras estar facinado con los dos anteriores libros que leí de Fernando Sábater, deseba leer mas"," La política es importante, bien o mal dependemos de ella y al ser dependientes y partes de una sociedad es nuestra responsabilidad ser parte activa de la política ") ;
+
+$books[5]=new Book("" , "" , "Español" , true , "" , "") ;
+
+$books[6]=new Book("" , "" , "Español" , true , "" , "") ;
+
+$books[8]=new Book("Revolución Hoy!" , "Guillermo Briseño" , "Español" , true , "Mi padre me dio este libro al ver que me últimamente me dedicaba bastante a la lectura" , "No recuerdo gran valor aportado por este libro, mas halla de que parace que el autor dice lo capo que es al convencer a la gente con el mensaje de Dios, y lo mucho que esto afecta en la vida de las personas. Lo raro es que no menciona explicitamente mejoras mas halla de -Que en general la gente estaba mas feliz porque hallo la verdad de Dios-") ;
+
+$books[9]=new Book("Juego de tronos" , "George R. R. Martin" , "Español" , true , "A partir de cierto punto la sere comenzó a desepcioarme específicamente en la 7ma temporada, me negaba a creer que tan buena historia se volviera así, asi que decidí comprarme el libro" , "Nunca antes había me había pasado horas literalmente horas leyendo un libro, me consta que este libro es mas que nada entretenimiento. Pero a pesar de todo este libro afirmo mi gusto por la lectura") ;
+
+$books[10]=new Book("Choque de reyes" , "George R. R. Martin" , "Español" , true , "Juego de tronos resulto ser igual que la serie, pensaba si en que en este libro se distanciaaría de la serie de tv" , "Alucinante, si se ven variaciones con la serie pero nada grave") ;
+
+$books[11]=new Book("Choque de espadas" , "George R. R. Martin" , "Español" , true , "No pude esperar a leer este libro" , "Aquí las cosas comenzaron a ser mucho mas diferente con la serie, y la verdad el libro empezaba a ser mas estructurado que la serie") ;
+
+$books[12]=new Book("Danza de cuervos" , "George R. R. Martin" , "Español" , true , "Me esperaba algo bueno" , "Y fue algo muy bueno, muy sorprendente en este punto claro que prefería el libro a la serie") ;
+
+$books[13]=new Book("El arte de la guerra - Audio libro " , "Sun Tzu" , "Español" , true , "En Youtube hay de todo y me encontre esto, la verdad pensaba que era una novela" , "A pesar de ser un libro antiguísimo Sun Tzu enseña estragias de un buen comandante en el arte de la guerra, enseñanzas que de alguna forma se pueden extrapolar a otros ámbitos de la vida" ) ;
+
+$books[14]=new Book("Danza de dragones" , "George R. R. Martin" , "Español" , False , "A falta de un pdf no lo leo aún" , "") ;
+
+$books[15]=new Book("La comunidad del aillo" , "J.R.R. Tolkien" , "Español" , False , "Si Cancion de Hielo y Fuego es mejor que la serie me pregunto que será de El señor de los anillos" , "") ;
+
+$books[16]=new Book("Las dos torres" , "J.R.R. Tolkien" , "Español" , False , "..." , "") ;
+
+$books[17]=new Book("El retorno del Rey" , "J.R.R. Tolkien" , "Español" , False ,"..." , "") ;
+
+$books[18]=new Book("Los hombres son de Marte y las mujeres de Venus" , "John Gray" , "Español" , True , "Buscaba un libro que me ayude a entender la situación de mi actual relación" , "Hay tanto que aprender de este libro que la verdad todo el planeta debería leerlo, pero lo mas importante que enseña es que tanto hombres como mujeres somos distintos") ;
+
+$books[19]=new Book("Influence" , "Robert Cialdini" , "English" , False , "Freddy Vega recomended this book on YouTube, I want to read it on english to improve my skills in english" , "") ;
+
+$books[20]=new Book("" , "George R. R. Martin" , "Español" , False , "" , "") ;
