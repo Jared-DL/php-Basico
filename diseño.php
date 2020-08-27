e18955d463a51c97f2cb3efa0f8356a111bee38a
Jared DL <davisleyton@outlook.com>
2020-08-27 18:12:41 -0400

    Añadí explícitamente que envié trabajo a git hub

--- diseño.php
+++ diseño.php
@@ -5,6 +5,7 @@
     </head>
     <body>
         <h1>Libros que quiero leer</h1>
+        <p>Acabo de enviar mi trabajo a github :v</p>
         <p> A continuación se desarrollará un pequeño sitio web con todos los libros que quiera leer en un futuro, además de los libros que ya leí. </p>
         
         <?php
